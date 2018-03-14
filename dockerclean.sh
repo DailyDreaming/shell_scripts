46271525d22c2729c68b2738bf916505f96d0ead
DailyDreaming <lblauvel@ucsc.edu>
2018-03-13 19:11:19 -0700

    Update dockerclean.sh

--- dockerclean.sh
+++ dockerclean.sh
@@ -7,4 +7,4 @@ docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
 
 # remove unused volumes:
-find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)') | xargs -r rm -fr
+sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)') | xargs -r rm -fr
