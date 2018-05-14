be1956c0e83e3be9c66847017f2726708e367e5b
DailyDreaming <lblauvel@ucsc.edu>
2018-05-14 14:27:45 -0700

    Update newsystem.sh

--- newsystem.sh
+++ newsystem.sh
@@ -60,6 +60,16 @@ sudo apt-get purge avahi-daemon
 # install video player codecs
 sudo apt-get install ubuntu-restricted-extras
 
+## https://cloud.google.com/sdk/docs/quickstart-debian-ubuntu
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
 # git
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
