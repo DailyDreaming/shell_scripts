3be916052fd491f0a8173abf8999a925967ca9f6
DailyDreaming <lblauvel@ucsc.edu>
2018-05-29 17:40:46 -0700

    Update newsystem.sh

--- newsystem.sh
+++ newsystem.sh
@@ -14,6 +14,7 @@ sudo apt install -y compizconfig-settings-manager
 sudo apt install -y gconf-editor
 sudo apt install -y default-jdk
 sudo apt install -y mysql-server libmysqlclient-dev
+sudo apt install -y ncbi-blast+
 
 # install docker
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
