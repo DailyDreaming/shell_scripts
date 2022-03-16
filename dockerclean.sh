9fd05524fb00df3dac12a7c8b338afc091d3b462
Lon Blauvelt <lblauvel@ucsc.edu>
2022-03-16 16:56:20 -0700

    Update dockerclean.sh

--- dockerclean.sh
+++ dockerclean.sh
@@ -8,3 +8,6 @@ docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker 
 
 # remove unused volumes:
 sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)') | xargs -r rm -fr
+
+# all
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v && docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi && sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)') | xargs -r rm -fr
